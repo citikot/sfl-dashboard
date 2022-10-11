5c3a0417df7d0ea06c16eab3571841abbf7aa2c3
Alexander Pishvanov <apishvanov@gmail.com>
2022-10-11 15:54:16 +0300

    database update 11-10-22

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ import pandas as pd
 from PIL import Image
 
 
-def switch_chart_data(period):
-    if period == "daily":
+def switch_chart_data(per):
+    if per == "daily":
         return "database/sfl_all_trans_daily_db_for_dashboard.parquet.gzip"
-    elif period == "weekly":
+    elif per == "weekly":
         return "database/sfl_all_trans_weekly_db_for_dashboard.parquet.gzip"
-    elif period == "monthly":
+    elif per == "monthly":
         return "database/sfl_all_trans_monthly_db_for_dashboard.parquet.gzip"
+
 
 st.set_page_config(layout="wide")
 st.sidebar.header("SFL game statistics test dashboard")
 img = Image.open("img/black_market.3bde9316.jpg")
 st.sidebar.image(img, width=300)
-
 
 st.title("Transactions amount chart")
 period = ['daily', 'weekly', 'monthly']
@@ -25,5 +25,3 @@ df = pd.read_parquet(db_file)
 
 with st.container():
     st.bar_chart(df['transactions'], use_container_width=True)
-
-
